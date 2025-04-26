e277392310edaa77b6f27a9e22e64e7539b15937
Dev546323 <devhirawat18@gmail.com>
2025-04-26 22:15:47 +0530

    modified:   inheritance 	modified:   inheritance.cpp 	modified:   inheritance.h

--- inheritance.cpp
+++ inheritance.cpp
@@ -8,6 +8,9 @@
 #include<string>
 #include<concepts>
 #include "inheritance.h"
+#include <vector>
+
+//Doesn't work, because i'm trying to use a loop to create objects 
 
 using namespace std;
 inline  void end(){
@@ -15,16 +18,98 @@ inline  void end(){
 }
 
 class person;
-class game; 
+class promotion;
+
 int main(){
-    cout<<"Please Enter your First name: ";
-    string fname,lname;
-    cin>>fname;
-    cout<<"\nPlease Enter your Last name: ";
-    cin>>lname;
-    cout<<"\nPlease Enter your age: ";
-    int age;
-    cin>>age;
-    person p1(fname,lname,age);
-    p1.display();
+    // cout<<"Please Enter your First name: ";
+    // string fname,lname;
+    // cin>>fname;
+    // cout<<"\nPlease Enter your Last name: ";
+    // cin>>lname;
+    // cout<<"\nPlease Enter your age: ";
+    // int age;
+    // cin>>age;
+    // person p1(fname,lname,age);
+    // p1.display();
+    //  vector<person> p(100);
+    string name{};
+    int age{0};
+    int years{};
+    int contribution{};
+    int salary{};
+    double behavior{};
+    string post{};
+    int count =0;
+    promotion p1("Rohan", 36, 10, 50, 50000, 7.0, "Manager");
+    promotion p2("Ravi", 30, 6, 60, 60000, 6.0, "Senior Manager");
+    promotion p3("Ramesh", 28, 9, 40, 40000, 8.0, "Junior Manager");
+    // p1.promote();
+    // p2.promote();
+    // p3.promote();
+    p1.newdetails();
+    p2.newdetails();
+    p3.newdetails();
+   
+    // for(int i=0;i<100;i++){
+        
+    //     cout<<"Please Enter Employee name: ";
+    //     cin>>name;
+    //     if (name == "exit"){
+    //         break;}
+    //      p[i]=person (name);
+    //      count++;
+    // }
+    // for(int i=0;i<count;i++){
+    //     cout<<"Please Enter Employee age: ";
+    //     cin>>age;
+    //     if (age == 0){
+    //         break;}
+    //      p[i].setage(age);
+    // }
+    // for(int i=0;i<count;i++){
+    //     cout<<"Please Enter Employee years: ";
+    //     cin>>years;
+    //     if (years == 0){
+    //         break;}
+    //      p[i].setyears(years);
+    // }
+    // for(int i=0;i<count;i++){
+    //     cout<<"Please Enter Employee contribution: ";
+    //     cin>>contribution;
+    //     if (contribution == 0){
+    //         break;}
+    //      p[i].setcontribution(contribution);
+    // }
+    // for(int i=0;i<count;i++){
+    //     cout<<"Please Enter Employee salary: ";
+    //     cin>>salary;
+    //     if (salary == 0){
+    //         break;}
+    //      p[i].setsalary(salary);
+    // }
+    // for(int i=0;i<count;i++){
+    //     cout<<"Please Enter Employee behavior: ";
+    //     cin>>behavior;
+    //     if (behavior == 0){
+    //         break;}
+    //      p[i].setbehavior(behavior);
+    // }
+    // for(int i=0;i<count;i++){
+    //     cout<<"Please Enter Employee post: ";
+    //     cin>>post;
+    //     if (post == "exit"){
+    //         break;}
+    //      p[i].setpost(post);
+    // }
+    // vector<promotion> c(count);
+    // cout<<"checking for promotion";
+    // for(int i=0;i<count;i++){
+    //     //cout<<"checking for promotion";
+    //     c[i].promote();
+    //     c[i].newdetails();
+    // }
+  
+
+        
+
 }
--- inheritance.h
+++ inheritance.h
@@ -1,67 +1,161 @@
+#pragma once 
 #include<iostream>
 using namespace std;
 
 
-class person{
-    string fname{"Undefined"};
-    string lname{"Undefined"};
+class person {
+    string name{"Undefined"};
     int age{0};
+    int years{};
+    int contribution{};
+    int salary{};
+    double behavior{};
+    string post{};
+
 
     public:
-
+    string contri{};
     person()= default;
-
-        person  (string& f, string& l, int& a){
-            fname = f;
-            lname = l;
+        person  (string f, int a, int y, int c, int s, double b, string p){
+            name = f;
+            if (age<18){
+                age = a;
+               // cout<<"You are not eligible to work";
+            }
+           years = y;
+          contribution = c;
+            salary = s;
+            behavior = b;
+            post = p;
+           
+        }
+        person(string f){
+            name = f;
+        }
+    void setfname(string_view f){
+        name = f;
+    }
+        void setage(int a){
             age = a;
-            cout<<"string cons";
         }
-
-            person(string_view f, string_view l, int& a){
-                fname = f;
-                lname = l;
-                age = a;
-                cout<<"str view";
-            }
-                person(string_view& f){
-                    fname = f;
+            void setyears(int y){
+                if(y<0){
+                   // cout<<"Years cannot be negative";
                 }
-                    person(string_view& f, string_view& l){
-                        fname = f;
-                        lname = l;
+               else if(0<y && y<5){
+                    //cout<<"You are a fresher";
+                    years = y;
+               }
+                else if(5<y && y<10){
+                    //cout<<"You are a mid level employee";
+                    years = y;
+                }
+                else if(10<y && y<20){
+                   // cout<<"You are a senior level employee";
+                    years = y;
+                }
+                else if(y>20){
+                //cout<<"You are a veteran";
+                years= y;
                     }
-                        person(int& a){
-                            age = a;
+                }
+                
+                        void setcontribution(int c){
+                        if (c < 0) {
+                           // cout << "Contribution cannot be negative";
+                        } else if (c > 25 && c < 75) {
+                            //cout << "Good contribution but there's major room for improvement";
+                            contribution = c;
+                        } else if (c >= 75 && c < 100) {
+                           // cout << "Good contribution, we'll look into your promotion";
+                            contribution = c;
+                        } else if (c == 100) {
+                          //Excellent contribution, you are a star";
+                            contribution = c;
+                        } else if (c > 100) {
+                            //cout << "Impossible, you are a robot";
+                            contribution = c;
+                        } else {
+                           // cout << "You're fired.";
                         }
-    void setfname(string_view& f){
-        fname = f;
-    }
-        void setlname(string_view& l){
-            lname = l;
-        }
-            void setage(int& a){
-                age = a;
-            }
+                    }
+                        void setsalary(int s){
+                            salary = s;
+                        }
+                            void setbehavior(double b){
+                                if (b < 0) {
+                                   // cout << "behavior cannot be negative";
+                                } else if (b > 2.5 && b < 7.5) {
+                                   // cout << "Good behavior but there's major room for improvement";
+                                    behavior = b;
+                                } else if (b >= 7.5 && b < 10) {
+                                    //cout << "Good behavior, we'll look into your promotion";
+                                    behavior = b;
+                                } else if (b == 10) {
+                                   // cout << "Excellent behavior, you are a star";
+                                    behavior = b;
+                                } else if (b > 10) {
+                                  //  cout << "Impossible, you are a robot";
+                                } else {
+                                   // cout << "You're fired.";
+                                }
+                            }
+                                void setpost(string_view p){
+                                    post = p;
+                                }
+
     string getfname()const {
-        return fname;
+        return name;
     }
-        string getlname()const {
-            return lname;
-        }
             int getage()const {
                 return age;
             }
-            
+                 int getyears() const{
+                return years;
+                 }
+                    int getcontribution() {
+                        return contribution;
+                    } 
+                        int getsalary()const {
+                            return salary;
+                        }
+                            double getbehavior()const {
+                                return behavior;
+                            }
+                                string getpost()const {
+                                    return post;
+                                }
+
     void display(){
-        cout<<"First Name: "<<fname<<endl;
-        cout<<"Last Name: "<<lname<<endl;
+        cout<<"First Name: "<<name<<endl;
         cout<<"Age: "<<age<<endl;
     }
 
 };
+class promotion :public person{
+    public:
+    promotion()= default;
+    promotion(string f, int a, int y, int c, int s, double b, string p):person(f,a,y,c,s,b,p){}//constructor requires explicit declaration. 
+    
+    int promote(){
+        if(getage()>=25 && getyears()>=5 && getcontribution()>=50 && getsalary()>=50000 && getbehavior()>=5.0){
+            cout<<"You are eligible for promotion"<<endl;
+            return 1;
+        }
+        else{
+            cout<<"You are not eligible for promotion"<<endl;
+            return 0 ; 
+        }
+       
+    }
+    void newdetails(){
+        if(promote()==1){ //damn recursion here. 
+            setcontribution(0);
+            setbehavior(0);
+            setsalary(getsalary()*1.2);
+            cout<<getfname()<<" "<<getage()<<" "<<getyears()<<" "<<getcontribution()<<" "<<getsalary()<<" "<<getbehavior()<<" "<<getpost()<<endl;
 
-class player :/*player is inherting from*/ public person{ //syntax meaning. 
-    
-    
+        }
+    }
+
 };
